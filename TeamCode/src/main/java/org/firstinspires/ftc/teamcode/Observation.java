90d7c67f79bba761f83f50d2c0affdf2a45c39c4
ReallyBADatWritingCode <Am.ogh@outlook.com>
2025-02-15 06:02:40 -0800

    Update Observation.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Observation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Observation.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+@Autonomous
 public class Observation extends LinearOpMode {
     DcMotor LFMotor;
     DcMotor RFMotor;
